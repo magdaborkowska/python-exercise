e62d8b4fd5ab5c411f37a080d5793a1bf63ee4ac
magdaborkowska <54847767+magdaborkowska@users.noreply.github.com>
2019-09-05 13:47:38 +0200

    More exercises added.

--- exercises.py
+++ exercises.py
@@ -1,2 +1,18 @@
 print((9*7) + (5*7))
 print((17*6) - (9*7 + 5*7))
+
+x = 2
+y = x
+print(y)
+
+mv_population = 74728
+mv_area = 11.995
+mv_density = mv_population / mv_area
+print(mv_density)
+
+
+
+
+
+
+
